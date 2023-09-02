78dcc9ecbadc4e9acaa9dd70dfd8c58857286746
Lluís <lluis.revilla@gmail.com>
2023-09-02 11:41:14 +0200

    Start package

---
+++ R/get_token.R
@@ -0,0 +1,24 @@
+#' Get ESIOS token
+#'
+#' To get a token you need to ask for it.
+#'
+#' @param token The character string as a token
+#'
+#' @return Invisible the token
+#' @export
+#' @examples
+get_token <- function(token = NULL) {
+  if (!is.character(token) || length(token) > 1 || nchar(token) < 10) {
+    stop("Token should be a single element of at least 10 characters.")
+  }
+  if (is.null(token)) {
+    token <- Sys.getenv("ESIOS_TOKEN", NA_character_)
+  }
+
+  if (is.na(token)) {
+    stop("No token is provided and it couldn't find it as 'ESIOS_TOKEN'.")
+  }
+  invisible(token)
+}
+
+
