ee2177d56e39336cf1b50506b8f0c36f64084699
Lluís <lluis.revilla@gmail.com>
2023-09-25 23:37:29 +0200

    Change order of checks

--- R/get_token.R
+++ R/get_token.R
@@ -13,13 +13,14 @@ get_token <- function(token = NULL) {
     token <- Sys.getenv("ESIOS_TOKEN", NA_character_)
   }
 
+  if (is.na(token)) {
+    stop("No token is provided and it couldn't find it as 'ESIOS_TOKEN'.")
+  }
+
   if (!is.character(token) || length(token) > 1 || nchar(token) < 10) {
     stop("Token should be a single element of at least 10 characters.")
   }
 
-  if (is.na(token)) {
-    stop("No token is provided and it couldn't find it as 'ESIOS_TOKEN'.")
-  }
   .state$token <- token
   invisible(token)
 }
